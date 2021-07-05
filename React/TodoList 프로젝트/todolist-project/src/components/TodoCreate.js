40636080f747693ad10651e679d2ca659c578d9a
ekfka4863 <ekfka4863@gmail.com>
2021-07-05 20:39:35 +0900

    Commit: TodoList 만들기

---
+++ React/TodoList 프로젝트/todolist-project/src/components/TodoCreate.js
@@ -0,0 +1,106 @@
+import React, {useState} from 'react';
+import styled, {css} from 'styled-components';
+import {MdAdd} from 'react-icons/md';
+
+const CircleButton = styled.button`
+  background: #38d9a9;
+  &:hover {
+    background: #63e6be;
+  }
+  &:active {
+    background: #20c997;
+  }
+
+  z-index: 5;
+  cursor: pointer;
+  width: 80px;
+  height: 80px;
+  display: block;
+  align-items: center;
+  justify-content: center;
+  font-size: 60px;
+  position: absolute;
+  left: 50%;
+  bottom: 0px;
+  transform: translate(-50%, 50%);
+  color: white;
+  border-radius: 50%;
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: 0.125s all ease-in;  // 애니메이션 적용
+  ${props =>        
+    props.open &&   // 만약에 open이라는 값이 있으면... 아래와 같은 스타일링 적용 
+    css`
+      background: #ff6b6b;
+      &:hover {
+        background: #ff8787;
+      }
+      &:active {
+        background: #fa5252;
+      }
+      transform: translate(-50%, 50%) rotate(45deg);
+    `}
+`;
+
+// 새로운 할일을 적어 넣는 칸의 위치를 정하는 div
+const InsertFormPositioner = styled.div`
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+`;
+
+// 실제로 할일을 적어 넣는 입력칸이 있을 div를 스타일링 
+const InsertForm = styled.form`
+  background: #f8f9fa;
+  padding-left: 32px;
+  padding-top: 32px;
+  padding-right: 32px;
+  padding-bottom: 72px;
+
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+  border-top: 1px solid #e9ecef;
+`;
+
+// 실제로 할일을 적어 넣는 입력 칸
+const Input = styled.input`
+  padding: 12px;
+  border-radius: 4px;
+  border: 1px solid #dee2e6;
+  width: 100%;
+  outline: none;
+  font-size: 18px;
+  box-sizing: border-box;
+`;
+
+
+function TodoCreate() {
+	
+	const [open, setOpen] = useState(false);
+	
+	const onToggle = () => setOpen(!open);
+	// open 을 반전 시키는 함수 onToggle
+
+	return (
+		<>
+			{/* 만약에 open 값이 true라면 InsertFormPositioner 내부에 InsertForm을 렌더링 한다 */}
+			{open && (
+				<InsertFormPositioner>
+					<InsertForm>
+					<Input autoFocus placeholder="할 일을 입력 후, Enter 를 누르세요" />
+					</InsertForm>
+				</InsertFormPositioner>
+			)}
+			<CircleButton onClick={onToggle} open={open}>
+				<MdAdd />
+			</CircleButton>
+		</>
+	);
+}
+
+export default TodoCreate;
---
+++ React/TodoList 프로젝트/todolist-project/src/components/TodoItem.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+import {MdDone, MdDelete} from 'react-icons/md';
+
+const Remove = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: #dee2e6;
+	font-size: 24px;
+	cursor: pointer;
+	&:hover {
+		color: #ff6b6b;
+	}
+	display: none;
+`;
+
+const TodoItemBlock = styled.div`
+	display: flex;
+	align-items: center;
+	padding-top: 12px;
+	padding-bottom: 12px;
+	&:hover {     // 커서를 올렸을 때 아이콘이 나타나게 함.
+		${Remove} {  // 위의 Remove 컴포넌트를 가르킴
+			display: initial;
+		}
+	}
+`;
+
+const CheckCircle = styled.div`
+	width: 32px;
+	height: 32px;
+	border-radius: 16px;
+	border: 1px solid #ced4da;
+	font-size: 24px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-right: 20px;
+	cursor: pointer;
+	${props =>         // props로 받아오는 것은... 
+		props.done &&    // props.done을 받아오는데, 스타일링은 아래와 같이... 
+		css`
+			border: 1px solid #38d9a9;
+			color: #38d9a9;
+		`}
+`;
+
+const Text = styled.div`
+	// 기본적으로 Text 컴포넌트가 갖는 스타일링은 아래와 같지만
+	flex: 1;
+	font-size: 21px;
+	color: #495057;
+	${props =>       // 만약 props로 done을 받아오면 컬러를 약간 연한 회색으로 설정해 준다
+		props.done &&
+		css`
+			color: #ced4da;
+		`}
+`;
+
+
+function TodoItem({ id, done, text }) {
+	return (
+		<TodoItemBlock>
+			{/* 만약에 CheckCircle 안에 done 값이 있다면 MdDone 컴포넌트를 렌더링 해준다 */}
+			<CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+			<Text done={done}>{text}</Text>
+			<Remove>
+				<MdDelete />
+			</Remove>
+		</TodoItemBlock>
+	);
+}
+
+export default TodoItem; 
